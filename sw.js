6fa0f8f75e0f4fd0cdcf0551c7b5f107b1d40c31
Gweru-Tech <Gwerutechnology@gmail.com>
2025-10-04 12:14:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,21 +7,18 @@ const urlsToCache = [
     'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+            .then(response => {
+                return response || fetch(event.request);
             })
     );
 });
